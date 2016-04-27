704d99fa0177c3bb969a03430dae1f4c983b1312
HarrisonA <radiov@gmail.com>
2016-04-26 20:50:39 -0700

    fixed a few linting issues

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function Lease(unit, resident, rent, start, end) {
-    this.unit = unit;
-    this.resident = resident;
-    this.rent = rent;
-    this.start = start;
-    this.end = end;
+  this.unit = unit;
+  this.resident = resident;
+  this.rent = rent;
+  this.start = start;
+  this.end = end;
 }
 
 // Rentlytics Dogs 🐕
@@ -13,27 +13,25 @@ var LEASES = [
     new Lease('103', 'Zilla', 1945, new Date(2015, 0, 1), new Date(2016, 0, 31)),
     new Lease('104', 'Voltaire', 2100, new Date(2015, 2, 1), new Date(2016, 2, 31)),
     new Lease('105', 'Cooper', 1895, new Date(2014, 11, 1), new Date(2016, 0, 31)),
-    new Lease('106', 'Allie', 2150, new Date(2015, 1, 1), new Date(2016, 1, 28))
+    new Lease('106', 'Allie', 2150, new Date(2015, 1, 1), new Date(2016, 1, 28)),
 ];
 
 var mydate = new Date(2015, 0, 1);
 console.log(mydate.getFullYear());
 
 function total_rent_payments(leases, year, month) {
-    // Should return a sum of all rent payments for a month.    
-    var sum = 0;
-    var currentDate = new Date(year, month);
-    leases.forEach(function(lease){
+  // Should return a sum of all rent payments for a month.
+  var sum = 0;
+  var currentDate = new Date(year, month);
+  leases.forEach(function (lease) {
 
-        if (lease.start <= currentDate && lease.end >= currentDate){
-            sum+= lease.rent;
-        }
-    })
+    if (lease.start <= currentDate && lease.end >= currentDate) {
+      sum += lease.rent;
+    }
+  });
 
-    return sum;
+  return sum;
 }
-
-
 
 function expected_payments(leases, year, month) {
     // Should return a list of all units, residents, and rent payments for a month.
@@ -53,27 +51,26 @@ mocha.suite.emit('pre-require', this, 'solution', mocha);
 // TEST SUITE SETUP - END
 
 describe('Rentlytics Doghouse Leases', function () {
-    it('total_rent_payments', function () {
-        assert(total_rent_payments(LEASES, 2015, 1) == 10400);
-        assert(total_rent_payments(LEASES, 2015, 0) == 5990);
-    });
+  it('total_rent_payments', function () {
+      assert(total_rent_payments(LEASES, 2015, 1) == 10400);
+      assert(total_rent_payments(LEASES, 2015, 0) == 5990);
+  });
 
-    it('expected_payments', function () {
-        assert(expected_payments(LEASES, 2015, 1) == [
-                {'unit': '101', 'name': 'Bandito', 'rent': 2150},
-                {'unit': '103', 'name': 'Zilla', 'rent': 1945},
-                {'unit': '105', 'name': 'Cooper', 'rent': 1895}
-            ]);
-    });
+  it('expected_payments', function () {
+      assert(expected_payments(LEASES, 2015, 1) == [
+              {'unit': '101', 'name': 'Bandito', 'rent': 2150},
+              {'unit': '103', 'name': 'Zilla', 'rent': 1945},
+              {'unit': '105', 'name': 'Cooper', 'rent': 1895}
+          ]);
+  });
 
-    it('move_in_schedule', function () {
-        assert(move_in_schedule(LEASES, 2015, 1) == [
-                {'unit': '101', 'name': 'Bandito'},
-                {'unit': '103', 'name': 'Zilla'}
-            ]);
-    });
+  it('move_in_schedule', function () {
+      assert(move_in_schedule(LEASES, 2015, 1) == [
+              {'unit': '101', 'name': 'Bandito'},
+              {'unit': '103', 'name': 'Zilla'}
+          ]);
+  });
 });
-
 
 mocha.run(function () {
 });
